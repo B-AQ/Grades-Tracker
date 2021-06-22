eaf73bed35792a16d52d1859694ad1997ad64387
b-aq <bashar.3akleh_92@hotmail.com>
2021-06-22 13:28:48 +0300

    submited the exam

---
+++ app.js
@@ -0,0 +1,30 @@
+
+'use strict';
+
+let passingGrade =
+// Returns a random integer from 0 to 9:
+// Math.floor(Math.random() * 10);
+
+// function getRndInteger(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) ) + min;
+// }
+
+
+// Students=[];
+
+// function Grades (math, english, arabic, art, science){
+//   this.minGrade=avgMinNumber;
+//   this.maxFrade=100;
+
+// }
+
+let table = document.getElementById('myTable');
+let tableRow = document.createElement('tr');
+
+table.appendChild('tr');
+tableRow.textContent;
+console.log('tr');
+
+
+// localstorage.names = JSON.stringify(studendNames);
+
